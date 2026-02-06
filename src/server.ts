f012d9cede382ba6a0153eaa28ea354e20666b9e
abdul kabir yhaya <airagraphicsdesign@gmail.com>
2026-02-06 14:58:21 -0800

    feat: initialize Express server with security, CORS, routing, and error handling.

--- src/server.ts
+++ src/server.ts
@@ -81,5 +81,4 @@ app.listen(PORT, () => {
   console.log(`Server running `);
   // console.log(`🚀 Server running on http://localhost:${PORT}`);
   console.log(`📝 Environment: ${process.env.NODE_ENV || "development"}`);
-  // console.log(`🔒 Security headers enabled`);
 });
