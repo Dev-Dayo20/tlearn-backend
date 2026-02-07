0ddedc870eb51dae6973a596093cd672810e9506
abdul kabir yhaya <airagraphicsdesign@gmail.com>
2026-02-06 16:03:55 -0800

    Render issue

--- src/server.ts
+++ src/server.ts
@@ -81,5 +81,5 @@ app.listen(PORT, () => {
   console.log(`Server running `);
   // console.log(`🚀 Server running on http://localhost:${PORT}`);
   console.log(`📝 Environment: ${process.env.NODE_ENV || "development"}`);
-  // console.log(`🔒 Security headers enabled`);
+  console.log(`🔒 Security headers enabled`);
 });
